5af23e52d8b67ad83ea8cd887e1bb1e0df3c08d8
trelienn <2500004512@stu.iku.edu.tr>
2025-11-21 10:42:30 +0300

    Update PlayerHealth.cs

--- PlayerHealth.cs
+++ PlayerHealth.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHealth : MonoBehaviour
+[SeriliazeField] private class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
